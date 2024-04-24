5e0627a925ee41e3fa1ebb705e38aa5c2730859c
Polozov2005 <aleksandr.polozov.2005@gmail.com>
2024-04-24 17:45:59 +0700

    1

--- PhyLab7.py
+++ PhyLab7.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
 import pandas as pd
 import locale
 
@@ -7,16 +8,12 @@ import locale
 row = 20
 
 # Количество столбцов
-column = 3
+column = 5
 
 # Объявление таблицы
 A = np.zeros((row + 1, column + 1))
-for i in range(1, row + 1):
-    A[i, 0] = i
-for j in range(1, column + 1):
-    A[0, j] = j
 
-# 1. Напряжение, показываемое вольтметром, 10^(-3) В
+# 1. Напряжение между анодом и катодом, мВ
 A[1:row + 1, 1:1 + 1] = [
     [925],
     [875],
@@ -41,7 +38,7 @@ A[1:row + 1, 1:1 + 1] = [
 ]
 A[1:row + 1, 1:1 + 1] = - A[1:row + 1, 1:1 + 1]
 
-# 2. Анодный ток, 10^-6 А
+# 2. Анодный ток, мкА
 A[1:row + 1, 2:2 + 1] = [
     [2],
     [3],
@@ -65,27 +62,39 @@ A[1:row + 1, 2:2 + 1] = [
     [1943]
 ]
 
-# 3. Что-то, в чём-то
-# A[1:row + 1, 3:3 + 1] = np.log(A[1:row + 1, 2:2 + 1] / A[row, 2])
-# A[1:row + 1, 3:3 + 1] = np.log(A[1:row + 1, 2:2 + 1] / A[1, 2])
+# 3. Натуральный логарифм анодного тока, ln(мкА)
 A[1:row + 1, 3:3 + 1] = np.log(A[1:row + 1, 2:2 + 1])
+
+# 4. Погрешность измерения напряжения между анодом и катодом, мВ
+A[1:row + 1, 4:4 + 1] = 1
+
+# 5. Погрешность измерения натурального логарифма анодного тока, ln(мкА)
+A[1:row + 1, 5:5 + 1] = np.mean(A[1:row + 1, 3:3 + 1]) * A[1:row + 1, 4:4 + 1] / np.mean(A[1:row + 1, 1:1 + 1])
 
 # Вывод таблицы
 df_A = pd.DataFrame(A)
 df_A = df_A.drop(index=[0])
 df_A = df_A.drop(columns=[0])
-rounded_df_A = df_A.round(decimals=1)
+rounded_df_A = df_A.round(decimals=2)
 print(rounded_df_A)
 
-# x = A[1:row + 1, 1:1 + 1]
-# y = A[1:row + 1, 2:2 + 1]
-# plt.plot(x, y)
+# Настройки графика
+fig, ax = plt.subplots()
+locale.setlocale(locale.LC_NUMERIC, "de_RU")
+font = {'family': 'Times New Roman',
+        'size': 12}
+plt.rc('font', **font)
+ax.ticklabel_format(useLocale=True)
+ax.grid(linewidth = 0.3)
+ax.spines['left'].set_position('zero')
+ax.spines['right'].set_color('none')
+ax.yaxis.tick_left()
+ax.spines['bottom'].set_position('zero')
+ax.spines['top'].set_color('none')
+ax.xaxis.tick_bottom()
 
-# plt.show()
+ax.plot(A[1:row + 1, 1:1 + 1], A[1:row + 1, 3:3 + 1], color='black')
+plt.ylim(0)
 
-
-x = A[1:row + 1, 1:1 + 1]
-y = A[1:row + 1, 3:3 + 1]
-plt.plot(x, y)
-
+plt.savefig('graph.pdf')
 plt.show()
